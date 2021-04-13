aa4867c21743956bd5f5d5da6f7c31d1fedeb42a
Vinicius Rodrigues Rocha <viniciusr.rocha@outlook.com>
2021-04-13 15:46:33 -0300

    SwitchIfEmpty and ResponseStatusException

--- src/main/java/com/vinicius/webflux/service/AnimeService.java
+++ src/main/java/com/vinicius/webflux/service/AnimeService.java
@@ -3,7 +3,9 @@ package com.vinicius.webflux.service;
 import com.vinicius.webflux.domain.Anime;
 import com.vinicius.webflux.repository.AnimeRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -18,7 +20,12 @@ public class AnimeService {
     }
 
     public Mono<Anime> findById(Long id) {
-        return animeRepository.findById(id);
+        return animeRepository.findById(id)
+                .switchIfEmpty(monoResponseStatusNotFoundException()).log();
+    }
+
+    public <T> Mono<T> monoResponseStatusNotFoundException() {
+        return Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND, "Anime not found"));
     }
 
 }
